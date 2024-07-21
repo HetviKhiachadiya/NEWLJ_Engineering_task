9a507d4028b330999a58800f1a9bc3a2f97a7d18
Hetvi Khichadiya <khichadiyahetvi2003@gmail.com>
2024-07-21 21:05:11 +0530

    Initial commit

---
+++ Collage.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package collage;
+/**
+ *
+ * @author HETVI
+ */
+public class Collage {
+
+    
+    public static void main(String[] args) {
+       for(int i=1; i<=30; i+=2)
+       {
+           System.out.println(i);
+       }
+    }
+    
+}
---
+++ test.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package collage;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author HETVI
+ */
+public class test {
+    public static void main(String[] args)
+    {
+//        String name = "Helloo Hii";
+//        String name2 = "How are you?";
+//        System.out.println(name.length());
+//        System.out.println(name+name2);
+//        System.out.println(name2.charAt(8));
+//        String name3 = name.replace('e', 'i');
+//        System.out.println(name3);
+//        System.out.println(name);
+//        System.out.println(name.substring(0, 4));
+//        String[] name = new String[3];
+//        name[0] = "xyz";
+//        name[1] = "abc";
+//        name[2] = "123";
+//        System.out.println(name[1]);
+//        System.out.println(name.length);
+//        System.out.println(name[0]);
+//        Arrays.sort(name);
+//        System.out.println(name[0]);
+//        int[][] marks = {{1,2,3}, {9,8,7}};
+//        System.out.println(marks[0][2]);
+//        Casting
+//        int num = (int)35.65;
+//        Scanner sc = new Scanner(System.in);
+//        int num1 = sc.nextInt();
+//        for(int i=1; i<num1; i++)
+//        {
+//            System.out.println(i);
+//            i++;
+//        }
+    }
+    
+}
+
